86cf31ead3f769541d96ec9d513a412c78623f21
Adam <adam@phenaproxima.net>
2018-08-22 14:27:55 -0400

    Rename and consolidate workflow transitions (#64)

--- modules/lightning_scheduler/tests/features/transitions.feature
+++ modules/lightning_scheduler/tests/features/transitions.feature
@@ -2,7 +2,7 @@
 Feature: Scheduling transitions on content
 
   Background:
-    Given I am logged in as a user with the "create page content, view own unpublished content, edit own page content, use editorial transition create_new_draft, use editorial transition review, use editorial transition publish, use editorial transition archive, use editorial transition archived_draft, schedule editorial transition publish, schedule editorial transition archive, view latest version" permissions
+    Given I am logged in as a user with the "create page content, view own unpublished content, edit own page content, use editorial transition create_new_draft, use editorial transition review, use editorial transition publish, use editorial transition archive, schedule editorial transition publish, schedule editorial transition archive, view latest version" permissions
     And I visit "/node/add/page"
     And I enter "Schedule This" for "Title"
 
--- src/Update/Update230.php
+++ src/Update/Update230.php
@@ -3,14 +3,20 @@
 namespace Drupal\lightning_workflow\Update;
 
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Extension\ModuleInstallerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\StringTranslation\TranslationInterface;
 use Drupal\lightning_roles\ContentRoleManager;
+use Symfony\Component\Console\Style\StyleInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * @Update("2.3.0")
  */
 final class Update230 implements ContainerInjectionInterface {
+
+  use StringTranslationTrait;
 
   /**
    * The module installer.
@@ -27,15 +33,30 @@ final class Update230 implements ContainerInjectionInterface {
   private $contentRoleManager;
 
   /**
+   * The workflow entity storage handler.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  private $workflowStorage;
+
+  /**
    * Update230 constructor.
    *
    * @param \Drupal\Core\Extension\ModuleInstallerInterface $module_installer
-   *   The module installer.
+   *   The module installer service.
+   * @param EntityStorageInterface $workflow_storage
+   *   The workflow entity storage handler.
+   * @param TranslationInterface $translation
+   *   (optional) The string translation service.
    * @param \Drupal\lightning_roles\ContentRoleManager
-   *   (optional) The content role manager.
+   *   (optional) The content role manager service.
    */
-  public function __construct(ModuleInstallerInterface $module_installer, ContentRoleManager $content_role_manager = NULL) {
+  public function __construct(ModuleInstallerInterface $module_installer, EntityStorageInterface $workflow_storage, TranslationInterface $translation = NULL, ContentRoleManager $content_role_manager = NULL) {
     $this->moduleInstaller = $module_installer;
+    $this->workflowStorage = $workflow_storage;
+    if ($translation) {
+      $this->setStringTranslation($translation);
+    }
     $this->contentRoleManager = $content_role_manager;
   }
 
@@ -51,6 +72,8 @@ final class Update230 implements ContainerInjectionInterface {
 
     return new static(
       $container->get('module_installer'),
+      $container->get('entity_type.manager')->getStorage('workflow'),
+      $container->get('string_translation'),
       $content_role_manager
     );
   }
@@ -76,6 +99,69 @@ final class Update230 implements ContainerInjectionInterface {
           'use moderation sidebar',
         ]);
     }
+  }
+
+  /**
+   * Alters editorial workflow transitions.
+   *
+   * @update
+   */
+  public function alterTransitions(StyleInterface $io) {
+    /** @var \Drupal\workflows\WorkflowInterface $workflow */
+    $workflow = $this->workflowStorage->load('editorial');
+    if (empty($workflow)) {
+      return;
+    }
+
+    $plugin = $workflow->getTypePlugin();
+    $configuration = $plugin->getConfiguration();
+
+    if ($plugin->hasTransitionFromStateToState('draft', 'review')) {
+      $transition = $plugin->getTransitionFromStateToState('draft',  'review');
+
+      $question = $this->t('Do you want to rename the "@draft_review" editorial workflow transition to "Send to review"?', [
+        '@draft_review' => $transition->label(),
+      ]);
+      if ($io->confirm($question)) {
+        $id = $transition->id();
+        $configuration['transitions'][$id]['label'] = 'Send to review';
+      }
+    }
+
+    if ($plugin->hasTransitionFromStateToState('archived', 'published')) {
+      $transition = $plugin->getTransitionFromStateToState('archived', 'published');
+
+      $question = $this->t('Do you want to rename the "@archived_published" editorial workflow transition to "Restore from archive"?', [
+        '@archived_published' => $transition->label(),
+      ]);
+      if ($io->confirm($question)) {
+        $id = $transition->id();
+        $configuration['transitions'][$id]['label'] = 'Restore from archive';
+      }
+    }
+
+    // Merge the archived_draft and create_new_draft transitions.
+    if ($plugin->hasTransition('archived_draft') && $plugin->hasTransition('create_new_draft')) {
+      $transition = $plugin->getTransition('create_new_draft');
+
+      // If the create_new_draft transition can already handle archived items,
+      // there's nothing to do. This is an edge case, but worth covering.
+      if (array_key_exists('archived', $transition->from())) {
+        return;
+      }
+
+      $question = $this->t('Do you want to allow the "@create_new_draft" editorial workflow transition to restore archived content into a draft state? This will remove the "@archived_draft" transition.', [
+        '@create_new_draft' => $transition->label(),
+        '@archived_draft' => $plugin->getTransition('archived_draft')->label(),
+      ]);
+      if ($io->confirm($question)) {
+        unset($configuration['transitions']['archived_draft']);
+        $configuration['transitions']['create_new_draft']['from'][] = 'archived';
+      }
+    }
+
+    $workflow->set('type_settings', $configuration);
+    $this->workflowStorage->save($workflow);
   }
 
 }
