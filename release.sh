2649d59b639c5bda2a4712add497b7e1fab94afb
Phéna Proxima <adam@phenaproxima.net>
2019-11-13 13:29:00 -0500

    [skip ci] Automated release script now tags semantic and drupal.org versions.

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Tags and pushes a release to drupal.org's git remote.
+# Tags and pushes a release to origin and drupal.org.
 # Example usage: ./release.sh 2.2.0 8.x-2.2
 
 SEMANTIC_VERSION=$1
@@ -18,17 +18,23 @@ if [[ $BRANCH =~ ^8\.x\-[0-9]+\.x$ ]]; then
   git remote add drupal-org git@git.drupal.org:project/$PROJECT.git
 
   git merge --squash $RELEASE_BRANCH
-  git commit --all --message "$SEMANTIC_VERSION Release"
-  git push origin
-  git push drupal-org
+  git commit --all --quiet --message "$SEMANTIC_VERSION Release"
+  git push --quiet origin
+  git push --quiet drupal-org
+  echo "Merged $RELEASE_BRANCH and pushed to origin and drupal.org."
 
   git branch -D $RELEASE_BRANCH
   git push origin :$RELEASE_BRANCH
+
+  git tag $SEMANTIC_VERSION
+  git push --quiet origin $SEMANTIC_VERSION
+  echo "Tagged $SEMANTIC_VERSION and pushed to origin."
+
   git tag $DRUPAL_VERSION
-  git push drupal-org $DRUPAL_VERSION
+  git push --quiet drupal-org $DRUPAL_VERSION
+  echo "Tagged $DRUPAL_VERSION and pushed to drupal.org."
 
   git remote remove drupal-org
-  echo "The tag $DRUPAL_VERSION has been pushed to drupal.org."
 
   if [[ $MARKDOWN ]]; then
     markdown logs/$DRUPAL_VERSION.md | pbcopy
