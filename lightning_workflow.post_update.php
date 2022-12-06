45fa391282cf775ecf7af6a9b3098d7edea987e7
Adam G-H <32250-phenaproxima@users.noreply.drupalcode.org>
2022-12-06 21:01:43 +0000

    Issue #3325663 by phenaproxima: Remove update paths and migration from Lightning Scheduler

--- lightning_workflow.post_update.php
+++ lightning_workflow.post_update.php
@@ -5,16 +5,11 @@
  * Contains post-update functions for Lightning Workflow.
  */
 
-use Drupal\lightning_core\ConfigHelper as Config;
-
 /**
- * Imports the moderated_content view.
+ * Implements hook_removed_post_updates().
  */
-function lightning_workflow_post_update_import_moderated_content_view() {
-  if (Drupal::moduleHandler()->moduleExists('views')) {
-    Config::forModule('content_moderation')
-      ->optional()
-      ->getEntity('view', 'moderated_content')
-      ->save();
-  }
+function lightning_workflow_removed_post_updates(): array {
+  return [
+    'lightning_workflow_post_update_import_moderated_content_view' => '4.0.0',
+  ];
 }
